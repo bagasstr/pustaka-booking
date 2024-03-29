c9185081a1f3fb7de295ac6014c8e604e8f8a975
bagasstr <bagassatrio2805@gmail.com>
2024-03-30 03:30:55 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Bagas Satrio P
+    Saya tinggal di daerah Tangerang
+    makanan favorit saya semua makanan";
+}
+}
